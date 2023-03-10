1e89bac9393cb08a17f335174d55839180568d32
DragonT13 <alex23456@naver.com>
2023-03-10 23:56:13 +0900

    SentimentAnalysis wip

--- src/SentimentAnalysis/MainClass.cs
+++ src/SentimentAnalysis/MainClass.cs
@@ -7,14 +7,12 @@ namespace SentimentAnalysis
 	{
 		public static void Main(string[] args)
 		{
-			// 파일 읽기, 문자열 토큰화
-			// 900개 리뷰파일 읽기
-			string[] text = new string[900];
-			for (int i = 0; i < 900; i++)
-			{
-				text[i] = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\negative_reviews\cv" + i + ".txt");
-			}
-			// 단어 세기
+			// 900개 리뷰파일 읽기 (파일 읽기)
+			string negativeFileList = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\negative_file_list.txt");
+			string positiveFileList = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\positive_file_list.txt");
+
+
+			// 리뷰에 포함된 단어 세기 (문자열 토큰화)
 
 
 			// 리뷰에서 한 번도 나타나지 않은 단어 삭제 & 단어 카운트 저장 (파일 쓰기)
@@ -22,7 +20,9 @@ namespace SentimentAnalysis
 
 			// positive, negative 분류 알고리즘 구현 ( 입력: 리뷰 파일, 단어 리스트)
 
+			// 10개의 리뷰 파일을 positive, negative, don't know로 분류
 
+			// 결과를 result.txt에 저장
 		}
 	}
 }
