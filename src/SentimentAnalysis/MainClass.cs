d1c3d6ef3504c8438f43feabf40b9d5b6abd6d6d
DragonT13 <alex23456@naver.com>
2023-03-11 23:58:52 +0900

    SentimentAnalysis wip

--- src/SentimentAnalysis/MainClass.cs
+++ src/SentimentAnalysis/MainClass.cs
@@ -7,10 +7,39 @@ namespace SentimentAnalysis
 	{
 		public static void Main(string[] args)
 		{
-			// 900개 리뷰파일 읽기 (파일 읽기)
-			string negativeFileList = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\negative_file_list.txt");
-			string positiveFileList = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\positive_file_list.txt");
+			// list.txt 내용 저장
+			string[] negativeFileList = new string[900];
+			string[] positiveFileList = new string[900];
 
+			int count = 0;
+			foreach (string line in File.ReadLines(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\negative_file_list.txt"))
+			{
+				negativeFileList[count] = line;
+				count++;
+			}
+
+			count = 0;
+			foreach (string line in File.ReadLines(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\positive_file_list.txt"))
+			{
+				positiveFileList[count] = line;
+				count++;
+			}
+
+			// 900개 리뷰파일 읽기 (파일 읽기)
+			string[] negativeReviews = new string[900];
+			string[] positiveReviews = new string[900];
+
+			for (int i = 0; i < negativeReviews.Length; i++)
+			{
+				negativeReviews[i] = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\" + negativeFileList[i]);
+			}
+
+			for (int i = 0; i < positiveReviews.Length; i++)
+			{
+				positiveReviews[i] = File.ReadAllText(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\" + positiveFileList[i]);
+			}
+
+			//File.ReadAllLines(@"C:\Users\YongHo\SentimentAnalysis\첨부파일\과제3\" + negativeFileList[0]);
 
 			// 리뷰에 포함된 단어 세기 (문자열 토큰화)
 
