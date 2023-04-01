14ed4fe6dd5515d71814bff760d1b341a6f1c85f
DragonT13 <alex23456@naver.com>
2023-04-01 21:21:42 +0900

    코드 정리 및 수정, 주석 정리

--- src/SentimentAnalysis/MainClass.cs
+++ src/SentimentAnalysis/MainClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 
 namespace SentimentAnalysis
@@ -10,11 +11,10 @@ namespace SentimentAnalysis
 		{
 			string fileLocation = "C:\\Users\\YongHo\\SentimentAnalysis\\첨부파일\\과제3\\";
 
-			// list.txt 내용 저장
+			int count = 0;
 			string[] negativeFileList = new string[900];
 			string[] positiveFileList = new string[900];
 
-			int count = 0;
 			foreach (string line in File.ReadLines(fileLocation + @"negative_file_list.txt"))
 			{
 				negativeFileList[count] = line;
@@ -28,7 +28,6 @@ namespace SentimentAnalysis
 				count++;
 			}
 
-			// 900개 리뷰파일 읽기 (파일 읽기)
 			string[] negativeReviews = new string[900];
 			string[] positiveReviews = new string[900];
 
@@ -44,29 +43,29 @@ namespace SentimentAnalysis
 				positiveReviews[i] = Regex.Replace(positiveReviews[i], @"\d", "");
 			}
 
-			// 백과사전 불러오기(Map, Dictionary)
 			Dictionary<string, int> negativeWords = new Dictionary<string, int>();
 			string[] negativeWordsList = File.ReadAllLines(fileLocation + @"negative-words.txt");
+
+			Dictionary<string, int> positiveWords = new Dictionary<string, int>();
+			string[] positiveWordsList = File.ReadAllLines(fileLocation + @"positive-words.txt");
+
 			foreach (string word in negativeWordsList)
 			{
 				negativeWords.Add(word, 0);
 			}
 
-			Dictionary<string, int> positiveWords = new Dictionary<string, int>();
-			string[] positiveWordsList = File.ReadAllLines(fileLocation + @"positive-words.txt");
 			foreach (string word in positiveWordsList)
 			{
 				positiveWords.Add(word, 0);
 			}
 
-			// 부정 리뷰에 포함된 모든 단어 세기 (문자열 토큰화)
+			// 나만의 부정 단어장 만들기
 			for (int i = 0; i < negativeReviews.Length; i++)
 			{
 				string[] negativeReviewWords = negativeReviews[i].Split(
 					new char[] { '.', '?', '!', ' ', ';', ':', ',', '(', ')', '/', '-', '"', '*', '\n' },
 					StringSplitOptions.RemoveEmptyEntries);
 
-				// 백과사전에 있는 단어를 토대로 나만의 백과사전 만들기 and 단어 수 세기 (C# Map)
 				for (int j = 0; j < negativeReviewWords.Length; j++)
 				{
 					if (negativeWords.ContainsKey(negativeReviewWords[j]))
@@ -75,36 +74,22 @@ namespace SentimentAnalysis
 					}
 				}
 			}
-			// value값이 0인 단어 삭제
-			List<string> NegativeKeyList = new List<string>();
 
-			foreach (KeyValuePair<string, int> item in negativeWords)
+			foreach (KeyValuePair<string, int> removeWords in negativeWords)
 			{
-				if (item.Value == 0)
+				if (removeWords.Value == 0)
 				{
-					NegativeKeyList.Add(item.Key);
+					negativeWords.Remove(removeWords.Key);
 				}
 			}
 
-			foreach (string key in NegativeKeyList)
-			{
-				negativeWords.Remove(key);
-			}
-
-			// 나만의 부정단어장(negativeWords) 출력
-			foreach (KeyValuePair<string, int> item in negativeWords)
-			{
-				Console.WriteLine(item.Key + " " + item.Value);
-			}
-
-			// 긍정 리뷰에 포함된 모든 단어 세기 (문자열 토큰화)
+			// 나만의 긍정 단어장 만들기
 			for (int i = 0; i < positiveReviews.Length; i++)
 			{
 				string[] positiveReviewWords = positiveReviews[i].Split(
 					new char[] { '.', '?', '!', ' ', ';', ':', ',', '(', ')', '/', '-', '"', '*', '\n' },
 					StringSplitOptions.RemoveEmptyEntries);
 
-				// 백과사전에 있는 단어를 토대로 나만의 백과사전 만들기 and 단어 수 세기 (C# Map)
 				for (int j = 0; j < positiveReviewWords.Length; j++)
 				{
 					if (positiveWords.ContainsKey(positiveReviewWords[j]))
@@ -114,31 +99,32 @@ namespace SentimentAnalysis
 				}
 			}
 
-			// value값이 0인 단어 삭제
-			List<string> PositiveKeyList = new List<string>();
-			foreach (KeyValuePair<string, int> item in positiveWords)
+			foreach (KeyValuePair<string, int> removeWords in positiveWords)
 			{
-				if (item.Value == 0)
+				if (removeWords.Value == 0)
 				{
-					PositiveKeyList.Add(item.Key);
+					positiveWords.Remove(removeWords.Key);
 				}
 			}
-			foreach (string key in PositiveKeyList)
-			{
-				positiveWords.Remove(key);
-			}
 
-			// 나만의 긍정단어장(positiveWords) 출력
+			// 나만의 부정 단어장(positiveWords) 출력
+			foreach (KeyValuePair<string, int> item in negativeWords)
+			{
+				Console.WriteLine(item.Key + " " + item.Value);
+			}
+			Console.WriteLine();
+
+			// 나만의 긍정 단어장(positiveWords) 출력
 			foreach (KeyValuePair<string, int> item in positiveWords)
 			{
 				Console.WriteLine(item.Key + " " + item.Value);
 			}
+			Console.WriteLine();
 
 			// 나만의 부정, 긍정 단어장 텍스트 파일로 저장(negative-sentiment, positive-sentiment)
 			string outputFilePath1 = fileLocation + "실제경과\\negative-sentiment.txt";
 			string outputFilePath2 = fileLocation + "실제경과\\positive-sentiment.txt";
 
-			// ***이 코드에 대해서 분석 필요***
 			using (StreamWriter writer = new StreamWriter(outputFilePath1))
 			{
 				foreach (KeyValuePair<string, int> item in negativeWords)
